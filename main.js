1ae36d9f1191c2ee1e88214e593229df7bad51b4
Asossosaror <assar.grimlund@gmail.com>
2023-06-27 16:23:19 +0200

    Basically finished with home page.

---
+++ main.js
@@ -0,0 +1,8 @@
+// Scroll to the top of the screen. This function is called when the arrow in the footer is clicked.
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+}
