c9d78ac7bea277038e2982d43da0517f3a7540ca
Asossosaror <assar.grimlund@gmail.com>
2023-06-27 16:56:22 +0200

    Finished the nav bar behaviour.

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+const navBar = document.querySelector('.nav-bar');
+const main = document.querySelector('main');
+
+// Make the navigationg bar slightly transparent when it is scrolled down from it's original state.
+window.addEventListener('scroll', () => {
+    const scrollPos = window.scrollY;
+    if(scrollPos > main.offsetTop) {
+        navBar.classList.add('nav-bar-transparent');
+    } else {
+        navBar.classList.remove('nav-bar-transparent');
+    }
+});
+
 // Scroll to the top of the screen. This function is called when the arrow in the footer is clicked.
 function scrollToTop() {
     window.scrollTo({
